b516ed13be21324d8e002a011caea36ca2c69154
Luke <86321570+luktowers@users.noreply.github.com>
2023-09-23 23:47:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,17 +13,21 @@ sudo apt install zsh -yq
 # Set Zsh as the default shell
 chsh -s $(which zsh) $(whoami)
 
-# Copy theme (assuming you've already downloaded or created oblivyun.zsh-theme)
-cp oblivyun.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/oblivyun.zsh-theme
-
 # Install Oh My Zsh (non-interactive)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Copy .zshrc configuration (assuming you've already created .zshrc)
-cp .zshrc ~/.zshrc
+# Download and copy theme
+curl -O https://raw.githubusercontent.com/luktowers/config/main/oblivyun.zsh-theme
+cp oblivyun.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/oblivyun.zsh-theme
 
 # Clone zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Clone zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Download and copy .zshrc configuration
+curl -O https://raw.githubusercontent.com/luktowers/config/main/.zshrc
+cp .zshrc ~/.zshrc
+
+source ~/.zshrc
