1f359cec7731cc74bd49e81a9e53508b93137860
Luke <86321570+luktowers@users.noreply.github.com>
2023-09-24 10:06:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,19 +16,19 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # Download and copy theme
 curl -O https://raw.githubusercontent.com/luktowers/config/main/oblivyun.zsh-theme
-cp oblivyun.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/oblivyun.zsh-theme
+mv oblivyun.zsh-theme ~/.oh-my-zsh/custom/themes/oblivyun.zsh-theme
 
 # Clone zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Clone zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
+sudo apt-get install ./exa_0.9.0-4_amd64.deb
 
 # Download and copy .zshrc configuration
 curl -O https://raw.githubusercontent.com/luktowers/config/main/.zshrc
 #cp .zshrc ~/.zshrc
-
-wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-sudo apt-get install ./exa_0.9.0-4_amd64.deb
 
 zsh -c 'source ~/.zshrc'
