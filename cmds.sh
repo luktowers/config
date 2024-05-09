65a230abaaea60f0de92f7647378bb4571d02a26
Luke <86321570+luktowers@users.noreply.github.com>
2024-05-09 00:16:58 +0000

    remove cmds

--- cmds.sh
+++
@@ -1,9 +0,0 @@
-ssh-keygen -t ed25519 -C "luke.ltrs@gmail.com"
-
-git config --global user.name "Luke"
-git config --global user.email "luke.ltrs@gmail.com"
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
